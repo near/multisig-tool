9f3cb612805fa2721f95a85bf3cd71d3cc6f3213
Illia Polosukhin <ilblackdragon@gmail.com>
2020-10-01 08:17:43 -0700

    Display mutlisig information

--- script.js
+++ script.js
@@ -22,21 +22,30 @@ window.onload = () => {
     })().catch(e => console.error(e));
 };
 
-async function loadAccounts() {
+function getAccounts() {
     let accountIds = window.localStorage.getItem('accounts');
+    if (accountIds) {
+        return accountIds.split(',');
+    }
+    return [];
+}
+
+function setAccounts(accountIds) {
+    console.log(accountIds);
+    window.localStorage.setItem('accounts', accountIds.join(','));
+}
+
+async function loadAccounts() {
+    let accountIds = getAccounts();
     console.log(`Accounts: ${accountIds}`);
     let accounts = [];
-    if (accountIds) {
-        accountIds = accountIds.split(',');
-        console.log(accountIds);
-        for (let i = 0; i < accountIds.length; ++i) {
-            const account = await window.near.account(accountIds[i]);
-            const state = await account.state();
-            accounts.push({ 
-                accountId: accountIds[i],
-                amount: nearAPI.utils.format.formatNearAmount(state.amount, 2)
-            });
-        }
+    for (let i = 0; i < accountIds.length; ++i) {
+        const account = await window.near.account(accountIds[i]);
+        const state = await account.state();
+        accounts.push({ 
+            accountId: accountIds[i],
+            amount: nearAPI.utils.format.formatNearAmount(state.amount, 2)
+        });
     }
     console.log(accounts);
     const template = document.getElementById('template1').innerHTML;
@@ -48,19 +57,27 @@ async function loadAccounts() {
 async function addAccount() {
     let accountId = document.querySelector('#account').value;
     console.log(`Adding ${accountId}`);
-    let accountIds = window.localStorage.getItem('accounts');
-    if (accountIds) {
-        accountIds += ',' + accountId;    
-    } else {
-        accountIds = accountId;
+    let accountIds = getAccounts();
+    if (!accountIds.includes(accountId)) {
+        accountIds.push(accountId);
+        setAccounts(accountIds);
     }
-    window.localStorage.setItem('accounts', accountIds);
     await loadAccounts();
     window.hash = accountId;
 }
 
 async function loadAccountDetails(accountId) {
+    document.getElementById('requests').innerHTML = 'loading';
+    let accountIds = getAccounts();
+    if (!accountIds.includes(accountId)) {
+        accountIds.push(accountId);
+        setAccounts(accountIds);
+        await loadAccounts();
+    }
     let contract = await window.near.account(accountId);
+    const numConfirmations = await contract.viewFunction(accountId, "get_num_confirmations", {});
+    const accessKeys = await contract.getAccessKeys();
+    console.log(accessKeys);
     const request_ids = await contract.viewFunction(accountId, "list_request_ids", {});
     let requests = [];
     for (let i = 0; i < request_ids.length; ++i) {
@@ -74,13 +91,30 @@ async function loadAccountDetails(accountId) {
     console.log(requests);
     const template = document.getElementById('template2').innerHTML;
     document.getElementById('requests').innerHTML = Mustache.render(template, {
+        accountId,
+        accessKeys,
+        numConfirmations,
         requests
     });
 }
 
+function confirmRequest(accountId, requestId) {
+    console.log(`Confirm ${accountId} request ${requestId}`);
+    alert(`Doesn't work yet`);
+}
+
+function submitRequest(requestKind) {
+    alert(`Doesn't work yet`);
+    if (requestKind == "add_key") {
+
+    }
+}
 
 window.nearAPI = nearAPI;
 window.addAccount = addAccount;
+window.confirmRequest = confirmRequest;
+window.submitRequest = submitRequest;
+
 window.onhashchange = () => {
     if (window.location.hash) {
         loadAccountDetails(window.location.hash.slice(1));
