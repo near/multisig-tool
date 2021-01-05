5d50fbca1bb0f9091074fcf89f2971e16d617f75
Felonius Gru <new-comet-dev@outlook.com>
2021-01-06 02:26:50 +0900

    - update app name - make babel configuration more extendable - add prettier configuration - update eslint ignore

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  plugins: [
-    ["@babel/plugin-syntax-optional-chaining"]
-  ],
-};
+const presets = [];
+const plugins = [["@babel/plugin-syntax-optional-chaining"]];
+
+if (process.env["ENV"] === "prod") {
+  // plugins.push(...);
+}
+
+module.exports = { presets, plugins };
