7b779d9ed65a3b1a3cee8b106578b79263aecec6
Felonius Gru <new-comet-dev@outlook.com>
2021-01-21 17:09:55 +0900

    disable typescript eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,11 @@ module.exports = {
   rules: {
     "no-plusplus": "off",
     'prettier/prettier': 0,
+    "no-param-reassign": 0,
+    "@typescript-eslint/ban-ts-comment": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "import/no-extraneous-dependencies": 0,
+    "no-console": 0
   }
 };
